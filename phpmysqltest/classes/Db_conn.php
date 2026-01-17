d3c99c9e424478aa1a3c07f555aa9470f028b4ce
rfrakeswcc <rfrakes@wccnet.edu>
2026-01-17 00:38:44 -0500

    updates

--- phpmysqltest/classes/Db_conn.php
+++ phpmysqltest/classes/Db_conn.php
@@ -9,9 +9,9 @@ class DatabaseConn {
     try {
 
       $dbHost = 'localhost';
-      $dbName = 'WCC Username';//this will be your WCC username
-      $dbUsr = 'WCC Username';//this will be your WCC username
-      $dbPass = 'Your password';//this will be the password found in the video.
+      $dbName = 'rfrakes';//this will be your WCC username
+      $dbUsr = 'rfrakes';//this will be your WCC username
+      $dbPass = 'wEJ3J8RoiZIjt4w';//this will be the password found in the video.
 
       $this->conn = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUsr, $dbPass);
       

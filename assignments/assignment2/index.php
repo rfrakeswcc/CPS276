2fd9a214488a419a921730247563031de10bfd8f
rfrakes <rfrakes@wccnet.edu>
2026-02-15 17:52:14 -0500

    updates

--- assignments/assignment2/index.php
+++ assignments/assignment2/index.php
@@ -1,15 +1,15 @@
-<?php
-    $numbers[];
-    for ( $i = 0; $i <= 49; $i++ )
-    {
-        array_push( $numbers, i );
-    }
-    foreach ( $numbers as $number )
-    {
-        echo $number;
-    }
-
-?>
+    <?php
+        $numbers = [];
+        for ( $i = 0; $i <= 49; $i++ )
+        {
+            $numbers[i] = ( $i + 1 );
+            echo $number;
+        }
+        foreach ( $numbers as $number )
+        {
+            echo $number;
+        }
+    ?>
 
 <!doctype html>
 <html lang="en">
@@ -20,9 +20,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
 <body class="container">
-  /*  <?php
+  <!--  <?php
         echo $evenNumbers;
         echo $form;
         echo createTable(8, 6);
-    ?> */
+    ?> -->
 </body>
